bfac32618adec32ae584bae489d61219679f6d03
Colin_lsplt <colinlespi@gmail.com>
2024-11-14 11:38:29 +0100

    navbar

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function toggleMenu() {
     const sideMenu = document.getElementById('sideMenu');
-    const hamburger = document.querySelector('.hamburger');
-
     sideMenu.classList.toggle('open');
-    hamburger.classList.toggle('open');
 }
