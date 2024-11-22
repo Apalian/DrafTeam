480bbb4b32a1575abba1c9421c73d5fef5dd8070
SirGhaT9wd <riyad.kssim@gmail.com>
2024-11-22 08:39:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+// Affichage des erreurs
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
+
 
 // Vérifie si l'utilisateur est connecté
 if (!isset($_SESSION['username']) || !isset($_SESSION['password'])) {
