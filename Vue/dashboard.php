cf1d1827f6764488a1a6e054d900a6dc483534dd
Colin_lsplt <colinlespi@gmail.com>
2024-11-22 08:43:10 +0100

    Update dashboard.php

--- Vue/dashboard.php
+++ Vue/dashboard.php
@@ -26,7 +26,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../styles.css">
     <title>DrafTeam</title>
 </head>
 <body>
