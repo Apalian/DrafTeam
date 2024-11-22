1bef582c9f86b9b6ee3758e0c911bda71f51e48c
Colin_lsplt <colinlespi@gmail.com>
2024-11-22 08:46:48 +0100

    Update dashboard.php

--- Vue/dashboard.php
+++ Vue/dashboard.php
@@ -1,4 +1,10 @@
 <?php
+
+// Affichage des erreurs sur Hostinger
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 
 // Vérification de la session
