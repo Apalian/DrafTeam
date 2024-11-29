4963d6bf9601d11d58e4477c94a9c02d61e4b0ae
SirGhaT9wd <riyad.kssim@gmail.com>
2024-11-29 09:17:54 +0100

    Update gestionMatchs.php

--- Vue/gestionMatchs.php
+++ Vue/gestionMatchs.php
@@ -105,7 +105,7 @@ if (isset($_GET['dateMatch'])&&isset($_GET['heure'])) {
                 </div>
                 <!-- Boutons Modifier et Supprimer -->
                 <div class="card-buttons">
-                    <a href="modifierMatch.php?numLicense=<?php echo $match->getDateMatch(); ?>"><button>Modifier</button></a>
+                    <a href="modifierMatch.php?dateMatch=<?php echo $match->getDateMatch() ?>"><button>Modifier</button></a>
                     <a href="?date=<?php echo $match->getDateMatch(); ?>&heure=<?php echo $match->getHeure(); ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce match ?');"><button>Supprimer</button></a>
                 </div>
             </div>
