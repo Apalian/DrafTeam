0a14b5cf83667a93bbc20a2e6f823cb5b7f9919d
Colin_lsplt <colinlespi@gmail.com>
2025-02-12 11:03:44 +0100

    Update index.php

--- API/Joueur/index.php
+++ API/Joueur/index.php
@@ -2,6 +2,10 @@
 require_once 'connexionDB.php';
 require_once 'functions.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
