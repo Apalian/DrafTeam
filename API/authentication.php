1800b103e8a758307a660021b631de2e3ba6f338
Colin_lsplt <colinlespi@gmail.com>
2025-02-14 10:47:10 +0100

    feat! : Setup Authentication

--- API/authentication.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-$mot_de_passe = '$iutinfo';
-$mot_de_passe_hache = password_hash($mot_de_passe, PASSWORD_BCRYPT);
-echo $mot_de_passe_hache;
-?>
