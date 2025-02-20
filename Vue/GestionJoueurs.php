9a550a9fcbc5743cb86e9c7853682c6a281457cb
Colin_lsplt <colinlespi@gmail.com>
2025-02-20 11:53:42 +0100

    Update GestionJoueurs.php

--- Vue/GestionJoueurs.php
+++ Vue/GestionJoueurs.php
@@ -29,20 +29,12 @@
     <div class="joueurs-list">
         <?php if (!empty($joueurs)): ?>
             <?php foreach ($joueurs as $joueur): ?>
-                <div class="card">
+                <div class="card compact-card">
                     <div class="card-body">
-                        <div class="card-left">
-                            <h2><?php echo htmlspecialchars($joueur->getNom()) . ' ' . htmlspecialchars($joueur->getPrenom()); ?></h2>
-                            <p><strong>Numéro de Licence:</strong> <?php echo htmlspecialchars($joueur->getNumLicense()); ?></p>
-                            <p><strong>Date de naissance:</strong> <?php echo htmlspecialchars($joueur->getDateNaissance()); ?></p>
-                            <p><strong>Statut:</strong> <?php echo htmlspecialchars($joueur->getStatut()); ?></p>
-                            <p><strong>Commentaire:</strong> <?php echo htmlspecialchars($joueur->getCommentaire()); ?></p>
-                        </div>
-
-                        <div class="card-right">
-                            <p><strong>Taille:</strong> <?php echo htmlspecialchars($joueur->getTaille()); ?>cm</p>
-                            <p><strong>Poids:</strong> <?php echo htmlspecialchars($joueur->getPoids()); ?>kg</p>
-                        </div>
+                        <h2><?php echo htmlspecialchars($joueur->getNom()) . ' ' . htmlspecialchars($joueur->getPrenom()); ?></h2>
+                        <p><strong>Numéro de Licence:</strong> <?php echo htmlspecialchars($joueur->getNumLicense()); ?></p>
+                        <p><strong>Statut:</strong> <?php echo htmlspecialchars($joueur->getStatut()); ?></p>
+                        <p><strong>Taille:</strong> <?php echo htmlspecialchars($joueur->getTaille()); ?>cm | <strong>Poids:</strong> <?php echo htmlspecialchars($joueur->getPoids()); ?>kg</p>
                     </div>
                     <div class="card-buttons">
                         <a href="../Controller/ModifierJoueurController.php?numLicense=<?php echo $joueur->getNumLicense(); ?>"><button>Modifier</button></a>
