27bcf9f9fa45922b7a3871cef86537eed645d69a
Colin_lsplt <colinlespi@gmail.com>
2025-03-05 17:06:06 +0100

    fix

--- JS/login.js
+++ JS/login.js
@@ -18,7 +18,7 @@ async function login(event) {
         // Stocker les informations dans le localStorage
         localStorage.setItem('username', username);
         localStorage.setItem('role', data.data.role);
-
+        localStorage.setItem('token', data.data.token);
         window.location.href = '../index.php'; // Rediriger vers la page d'accueil
     } else {
         // Afficher un message d'erreur
