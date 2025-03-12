1185abb273e23b023ad532ef2982c769983cfbc9
Colin_lsplt <colinlespi@gmail.com>
2025-03-12 14:11:09 +0100

    Update statistiques.js

--- JS/statistiques.js
+++ JS/statistiques.js
@@ -98,7 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!numLicense) return;
 
         try {
-            const response = await fetch(`https://drafteamapi.lespi.fr/Statistques?numLicense=${numLicense}`, {
+            const response = await fetch(`https://drafteamapi.lespi.fr/Statistques/index.php?numLicense=${numLicense}`, {
                 method: 'GET',
                 headers: {
                     'Authorization': `Bearer ${token}`
