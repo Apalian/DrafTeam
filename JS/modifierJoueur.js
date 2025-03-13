98b1dea899d96aaf1bfbf60140320ddfeaf614fd
Colin_lsplt <colinlespi@gmail.com>
2025-03-13 10:54:51 +0100

    Update modifierJoueur.js

--- JS/modifierJoueur.js
+++ JS/modifierJoueur.js
@@ -32,14 +32,15 @@ document.addEventListener("DOMContentLoaded", async () => {
     initialData = await response.json(); // Stocker les données initiales
     console.log(initialData);
     // Remplir les champs du formulaire avec les données du joueur
-    document.getElementById("nom").value = initialData.data.nom;
-    document.getElementById("prenom").value = initialData.data.prenom;
+    document.getElementById("nom").value = initialData.data[0].nom;
+    document.getElementById("prenom").value = initialData.data[0].prenom;
     document.getElementById("dateNaissance").value =
-      initialData.data.dateNaissance;
-    document.getElementById("statut").value = initialData.data.statuts;
-    document.getElementById("commentaire").value = initialData.data.commentaire;
-    document.getElementById("taille").value = initialData.data.taille;
-    document.getElementById("poids").value = initialData.data.poids;
+      initialData.data[0].dateNaissance;
+    document.getElementById("statut").value = initialData.data[0].statuts;
+    document.getElementById("commentaire").value =
+      initialData.data[0].commentaire;
+    document.getElementById("taille").value = initialData.data[0].taille;
+    document.getElementById("poids").value = initialData.data[0].poids;
   } catch (error) {
     console.error(
       "Erreur lors de la récupération des données du joueur:",
