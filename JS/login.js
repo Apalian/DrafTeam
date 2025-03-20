43fd2dfdf426ef21bad94b769146f8de4ece0368
Colin_lsplt <colinlespi@gmail.com>
2025-03-20 09:12:35 +0100

    test

--- JS/login.js
+++ JS/login.js
@@ -4,26 +4,28 @@ async function login(event) {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  const response = await fetch(
-    "https://drafteamauthentication.lespi.fr/authentication.php",
-    {
+  try {
+    const response = await fetch(`${API_CONFIG.AUTH_URL}/authentication.php`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ login: username, password: password }),
+    });
+
+    const data = await response.json();
+
+    if (data.status === "success") {
+      // Stocker les informations dans le localStorage
+      localStorage.setItem("username", username);
+      localStorage.setItem("token", data.data);
+      window.location.href = "../index.html"; // Rediriger vers la page d'accueil
+    } else {
+      // Afficher un message d'erreur
+      alert(data.status_message);
     }
-  );
-
-  const data = await response.json();
-
-  if (data.status === "success") {
-    // Stocker les informations dans le localStorage
-    localStorage.setItem("username", username);
-    localStorage.setItem("token", data.data);
-    window.location.href = "../index.html"; // Rediriger vers la page d'accueil
-  } else {
-    // Afficher un message d'erreur
-    alert(data.status_message);
+  } catch (error) {
+    console.error("Erreur lors de la connexion:", error);
+    alert("Une erreur est survenue lors de la connexion.");
   }
 }
--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
 function logout() {
   localStorage.clear();
-  window.location.href = "./Vue/Login.php"; // Assurez-vous que le chemin est correct
+  window.location.href = "./Vue/Login.html"; // Assurez-vous que le chemin est correct
 }
